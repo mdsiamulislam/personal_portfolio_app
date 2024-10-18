18e316798877234598e9088869b2c65a31980b75
Md Siamul Islam Soaib <mdsiamulislams@gmail.com>
2024-10-18 10:37:39 +0600

    Add those

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Personal Portfolio App',
+      title: 'Personal Portfolio',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
             seedColor: Colors.black,
